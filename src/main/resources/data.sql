b6cd3de6169bfa470b200a45a7decef386712192
Farith Sanmiguel <farith.sanmiguel@middlesoftware.com>
2018-07-04 17:29:51 -0500

    ajuste view

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-create table spydata (id bigint not null, characters varchar(1000), computer varchar(255), date date, primary key (id))
+create table spydata (id bigint not null, characters varchar(255), computer varchar(255), date date, primary key (id))
